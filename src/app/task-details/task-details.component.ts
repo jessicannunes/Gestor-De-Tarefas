6b46ee83bd9a1aa66f6128ebd96842c416167eb4
Jess Nunes <eng.diias@gmail.com>
2024-06-10 07:40:53 -0300

    Aplicação de funcionalidades  nos componentes

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Task } from '../Models/task';
+import { TaskService } from '../service/task.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-task-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.css'
 })
 export class TaskDetailsComponent {
+
+  task?: Task;
+
+  constructor(private router: Router, private route: ActivatedRoute, private taskService: TaskService){}
+
+  // assim que inicializar
+  ngOnInit(){
+    let id = this.route.snapshot.paramMap.get('id');
+
+    // verificação do Id da tarefa
+    if(id === null)
+    {
+
+      this.navigateBack();
+    }
+
+    else
+    {
+      this.task = this.taskService.getById(+id);
+
+      if(this.task === undefined){
+        this.navigateBack();
+      }
+      
+    }
+
+  }
+
+  // metodo para voltar a rota caso o id não seja encontrado
+  navigateBack(){
+    this.router.navigate(['/taskList'], {relativeTo: this.route});
+  }
+
+// botões salvar e cancelar
+  save(){
+    this.taskService.updateTask();
+    this.navigateBack();
+  }
+
+  cancel(){
+    this.navigateBack();
+  }
+
 
 }
--- src/app/task-filter/task-filter.component.ts
+++ src/app/task-filter/task-filter.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-filter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './task-filter.component.html',
   styleUrl: './task-filter.component.css'
 })
 export class TaskFilterComponent {
+
+  filter: string ='';
+
+  @Output() taskfilter = new EventEmitter<string>();
+
+  filterTasks(){
+
+    this.taskfilter.emit(this.filter);
+  }
 
 }
